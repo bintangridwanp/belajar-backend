2fcdb874af3b65f54fc617afd8e96d98d2ed385f
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-06-10 11:18:25 +0700

    Menambahakan 2 project

---
+++ tempat-kosong/contoh 2r/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using jurnal4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        KodeProduk produk = new KodeProduk();
+
+        Console.WriteLine(produk.getKodeProduk("Headset"));
+        Console.WriteLine(produk.getKodeProduk("Smartwatch"));
+
+        FanLaptop mode = new FanLaptop();
+
+        mode.modeUp();
+        mode.modeUp();
+        mode.modeUp();
+        mode.modeUp();
+        mode.offTurboShorcut();
+
+        mode.onTurboShorcut();
+        mode.modeDown();
+        mode.modeDown();
+        mode.modeDown();
+        mode.modeDown();
+        mode.onTurboShorcut();
+    }
+}
